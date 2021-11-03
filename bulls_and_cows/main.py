c30667b70a22d9c8232a4a5b27ddb5ee731e54ec
allprecisely <allprecisely@gmail.com>
2021-11-03 19:07:27 +0300

    edits + test

--- bulls_and_cows/main.py
+++ bulls_and_cows/main.py
@@ -1,81 +1,20 @@
 """
-Начнем отгадывать. Первые 4 цифры абсолютно не важны: это могут быть 1234 или 4 рандомных
-Собственно пусть в начале спросим 0123.
-Пользователь может ответить как угодно (парой б к):
-0 0; 0 1; 0 2; 0 3; 0 4; 1 0; 1 1; 1 2; 1 3; 2 0; 2 1; 2 2; 3 0; 4 0 - 14 вариантов
-если он отвечает любым из вариантов, то мы отбрасываем множество потенциальных
-- то есть нужно составить множество потенциальных вариантов - изначально это все 4 значные
-числа с неповторяющимися цифрами, которые могут содержать первым числом ноль
-всего вариантов 10 * 9 * 8 * 7 = 5040
-
-пусть вариант 0 0:
-тогда мы точно знаем, что можно отбросить все числа, содержащие любую из цифр
-останется 6 * 5 * 4 * 3 = 360
-
-пусть вариант 0 1:
-мы предположим, что 0 есть в числе. при этом 1 2 3 там нет; 0 точно есть и не на первой позиции
-6 * 5 * 4 * 3 = 360 вариантов (0 точно в числе в любой из 3 позиций; и остается 6 чисел по 3 позициям)
-360 * 4 = 1440 (но при этом это работает для 4 цифр)
-
-пусть 0 2:
-предполагаем, что в числе 0 1: тогда 2,3 там нет, и 0, 1 есть но не на своих позициях
-6 * 5 * 7 = 210 (7 вариантов расположить 0 и 1 по своим позициям; при этом остается 6 чисел в 2 п)
-210 * 6 = 1260 (6 пар можно взять)
-
-0 3:
-логика аналогична для 0 2, видим закономерность:
-- нужно посчитать сколькими способами можно выбрать С коров из N позиций, причем чтобы они не стояли
-на своих изначальных местах - это можно перебором с помощью функции
-- нужно посчитать, сколько остается цифр из А с учетом C коров, и сколькими способами их можно расставить
-на N - C позициях
-- сколько пар из С коров можно составить
-
-можно все легче, перебором
-например, 0123 - 0 1
-мб 0123? нет, т.к. коров и быков недостаточно. и так проверяем все числа, обновляя таблицу
-
-
-теперь перейдем к тому, как же определить, что именно спросить:
-для каждого существующего варианта по идее нужно узнать, какие ответы возможны, и сколько после них
-останется вариантов. самый перспективный нас и заинтересует
-например, после того, как нам ответили на 0123 - 0 1; у нас остается 1440 вариантов
-- мы знаем, что если мы спросим снова 0123, то в принципе мы не улучшим ситуацию
-- 0124: этот вариант возможен, и он нам может дать следующие ответы
-0 0; 0 1; 0 2; 1 0; 1 1
-при получении каждого ответа у нас сузится количество возможных ответов
-0 0 - 180
-0 1 - ... и т.д.
-в результате мы смотрим на медианное значение в случае больше 2 возможностей (среднее при 2, 1)
-и определяем какое число следующее
-- сложность: как определить количество вариантов?
-- сложность: 456789 - в данном случае равнозначные цифры, и нет смысла проверять 0125 если 0124 проверено
-
-можно предположить (сходу не знаю так ли это), что при одинаковых возможностях числа равнозначны
-(буду отталкиваться от этого предположения пока)
-
-в принципе можно сделать некрасивый дикт с ключами как тапл возможностей, а значения - сет чисел
-этот дикт нужно переформировывать при каждом вопросе
-кажется не очень дорого, но как забрать возможные ответы...
-можно пойти дорогим вариантом, проверяя все ответы, и исключать те, которые оставляют 0 вариантов
-(собственно при 0123 - 1 1: уже неприятно определять 0124: 0 1; 0 2; 1 0; 1 1; 1 2; 2 1 - логика
-сходу не улавливается; но можно все же в кэше сохранять возможные варианты - их будет становится
-же меньше только)
-в общем тогда для каждого числа храним список возможных значений
-при каждом вопросе проходимся снова по всей таблице, и удаляем лишнее
-при этом инициализируем 2 структуры: ключи, которые будем изучать, и таплы из списка значений
-если следующее значение из таблицы есть в структуре таплов, то скипаем значение
-
-
+1) Составляем таблицу возможных значений на основании ответов пользователя
+2) С учетом первого ответа пользователя, обновляем таблицу возможных значений 1
+3) Для каждого числа из ВСЕХ значений проверяем все возможные ответы пользователя,
+и выбираем то число, которое при любом ответе будет максимально уменьшать таблицу 1
+- оптимизация 1: запоминаем для каждого числа список возможных ответов, уменьшая его на каждой итерации
+- оптимизация 2: цифры, которые еще не спрашивались - равнозначны. варианты с ними схлопываются
+4) повторяем 2-3, пока пользователь не напишет 0 4 или в таблице 1 останется одно число
 """
 import logging
-import statistics
 import string
-from typing import Dict, List, Optional, Set, Tuple
+from typing import Any, Dict, List, Optional, Set, Tuple
 import utils
 
 N = 4
 ALNUM = tuple(string.digits)
-POSSIBLE_ANSWERS = [
+POSSIBLE_ANSWERS = {
     (0, 0),
     (0, 1),
     (0, 2),
@@ -90,7 +29,7 @@ POSSIBLE_ANSWERS = [
     (2, 2),
     (3, 0),
     (4, 0),
-]
+}
 logger = logging.getLogger()
 
 
@@ -121,23 +60,25 @@ def count_bulls_and_cows(guess: str, answer: str) -> Tuple[int, int]:
 
 
 def update_table_after_guess(
-    bulls: int, cows: int, guess: str, table: Set[str]
-) -> Set[str]:
-    new_table = set()
+    bulls: int, cows: int, guess: str, table: List[str]
+) -> List[str]:
+    new_table = []
     for variant in table:
         if (bulls, cows) == count_bulls_and_cows(guess, variant):
-            new_table.add(variant)
+            new_table.append(variant)
     return new_table
 
 
 def update_numbers_to_check(
     used_alnum: Tuple[str, ...],
     possible_answers: Dict[str, List[Tuple[int, int]]],
-    table: Set[str],
+    table: List[str],
 ) -> Dict[str, List[int]]:
     result = {}
     not_used_alnum = ALNUM[len(used_alnum) :][:4]
-    gen_table = generate_variants(alnum=used_alnum + ("*",) * len(not_used_alnum))
+    gen_table = sorted(
+        generate_variants(alnum=used_alnum + ("*",) * len(not_used_alnum))
+    )
     for number in gen_table:
         not_used_alnum_counter = 0
         new_variant = number
@@ -158,79 +99,81 @@ def update_numbers_to_check(
                 result[new_variant].append(len(n))
                 new_key_possible_answers.append(answer)
         possible_answers[new_variant] = new_key_possible_answers
-        if possible_answers[new_variant] == [(4, 0)]:
-            return {new_variant: [1]}
     return result
 
 
-def get_guess(check_these_numbers, questions) -> str:
-    best_key, best_median, best_mean = "", float("inf"), float("inf")
+def get_guess(check_these_numbers, possible_answers) -> str:
+    best_guess, best_val = "", float("inf")
     for number, remained_variants in check_these_numbers.items():
-        if number in questions:
-            continue
-        median = statistics.median(remained_variants)
-        if median < best_median:
-            logger.debug(
-                "=> %s %s %s %s",
-                number,
-                remained_variants,
-                median,
-                sum(remained_variants),
-            )
-
-            best_key, best_median, best_mean = (
-                number,
-                median,
-                statistics.mean(remained_variants),
-            )
-        elif median == best_median:
-            if mean := statistics.mean(remained_variants) < best_mean:
-                best_key, best_median, best_mean = number, median, mean
-    return best_key
+        max_val = max(remained_variants)
+        if max_val < best_val or (
+            max_val == best_val and (4, 0) in possible_answers[number]
+        ):
+            best_guess, best_val = number, max_val
+    return best_guess
 
 
-def get_initial_options(first_guess: str = "0123") -> Dict[Tuple[int, int], str]:
+def get_initial_options(guess: str = "0123") -> Any:
     result = {}
-    table = generate_variants()
-    possible_answers = {number: POSSIBLE_ANSWERS for number in table}
+    table = sorted(generate_variants())
     for answer in POSSIBLE_ANSWERS:
+        changed_table = update_table_after_guess(answer[0], answer[1], guess, table)
         check_these_numbers = update_numbers_to_check(
-            tuple(first_guess), possible_answers, table
+            tuple(guess), {number: POSSIBLE_ANSWERS for number in table}, changed_table
         )
-        result[answer] = get_guess(check_these_numbers, [])
+        result[answer] = changed_table, check_these_numbers
     return result
+
+
+def get_input(key: str, guess: str, table: List[str]) -> Any:
+    if key:
+        inp = " ".join(map(str, count_bulls_and_cows(guess, key)))
+        logger.debug(
+            "=> guess: %s; b c: %s; len: %s\n",
+            guess,
+            inp,
+            len(table),
+        )
+    else:
+        print(f'How many bulls and cows in "{guess}"?')
+        inp = input("bulls cows: ")
+        while True:
+            try:
+                if tuple(map(str, inp.split())) in POSSIBLE_ANSWERS:
+                    break
+            except:
+                pass
+            inp = input("type correct number of bulls and cows: ")
+
+    return map(int, inp.split())
 
 
 def main(
     key: Optional[str] = None,
 ):
     rounds = 0
-    table = generate_variants()
+    table = sorted(generate_variants())
     possible_answers = {number: POSSIBLE_ANSWERS for number in table}
     used_alnum = ("0", "1", "2", "3")
     check_these_numbers = {"0123": [0]}
-    questions = set()
     while True:
         rounds += 1
-        guess = get_guess(check_these_numbers, questions)
-        questions.add(guess)
-        used_alnum = tuple(set(used_alnum + tuple(guess)))
-
-        if key:
-            inp = " ".join(map(str, count_bulls_and_cows(guess, key)))
-            logger.debug("=========================\nguess: %s; b c: %s", guess, inp)
+        guess = get_guess(check_these_numbers, possible_answers)
+        if len(used_alnum) < 8:
+            used_alnum = tuple(set(used_alnum + tuple(guess)))
         else:
-            print(f'How many bulls and cows in "{guess}"?')
-            inp = input("bulls cows: ")
-            while tuple(map(int, inp.split())) not in POSSIBLE_ANSWERS:
-                print(f"your input is {tuple(inp.split())}")
-                inp = input("type correct number of bulls and cows: ")
+            used_alnum = ALNUM
 
-        bulls, cows = map(int, inp.split())
-        if bulls == N:
-            logger.debug(f"Won in {rounds}!")
+        while True:
+            bulls, cows = get_input(key, guess, table)
+            table = update_table_after_guess(bulls, cows, guess, table)
+            if table:
+                break
+            print('Enter valid answer.')
+        if bulls == 4 or len(table) == 1:
+            rounds += (bulls != 4)
+            logger.debug(f"Your num is {table.pop()}. Won in {rounds} rounds!")
             return rounds
-        table = update_table_after_guess(bulls, cows, guess, table)
 
         check_these_numbers = update_numbers_to_check(
             used_alnum, possible_answers, table
@@ -238,11 +181,10 @@ def main(
 
 
 if __name__ == "__main__":
-    # utils.init_logger('INFO')
-    main("5086")
-
-    # main_table = generate_variants()
-    # for i in main_table:
-    #     if j := main(i) > 7:
-    #         print(f'!!!!!!!!! {j} ', end=' ')
-    #     print(i)
+    utils.init_logger("INFO")
+    main_table = generate_variants()
+    for k, i in enumerate(sorted(main_table)[800:]):
+        if (j := main(i)) > 7:
+            print(f"!!!!!!!!! {j} {i}")
+        if not k % 50:
+            print(f"before {i} everything is ok, continue from {k}")
---
+++ bulls_and_cows/test_main.py
@@ -0,0 +1,32 @@
+import os
+import unittest
+
+import main
+
+
+class TestPreReqs(unittest.TestCase):
+    pass
+
+
+def create_test(case, func):
+    def do_test_expected(self):
+        self.assertLess(func(case), 8)
+    return do_test_expected
+
+
+def manipulations(cases, func):
+    for case in cases:
+        test_method = create_test(case, func)
+        test_method.__name__ = f'test_{case}'
+        setattr(TestPreReqs, test_method.__name__, test_method)
+
+
+if __name__ == '__main__':
+    all_cases = sorted(main.generate_variants())
+    first_case = os.getenv('FIRST_CASE', '0123')
+
+    manipulations(all_cases[all_cases.index(first_case):], main.main)
+    log_file = 'log_file.txt'
+    with open(log_file, "w") as f:
+        runner = unittest.TextTestRunner(f, verbosity=2)
+        unittest.main(testRunner=runner)
